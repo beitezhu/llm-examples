8a3599588e4f6af1207f6737456f0edc683e5d5b
guptad2 <guptadeepanshu25@gmail.com>
2024-04-27 14:13:59 -0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -8,7 +8,7 @@ with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
-st.title("Cartoon Memories my commi ")
+st.title("Momentoons")
 st.caption("🚀 AI-Powered Cartoon Video Maker for Memories")
 st.write("""
     ### Transform Your Memories into Cartoon Stories
